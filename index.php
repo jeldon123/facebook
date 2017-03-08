809c118705c6fe7a36bef246d9237619bceac24d
jeldon123 <jelsasukeops@gmail.com>
2017-03-09 00:35:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,14 @@ if($messageText == "sure?") {
     $answer = "lagi";
 }
 
+ if(event.message=='file'){
+    var fileObj = {
+     "type":"file", 
+     "url":"http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf"
+      };
+      context.sendResponse(JSON.stringify(fileObj));
+    }
+
 $response = [
     'recipient' => [ 'id' => $senderId ],
     'message' => [ 'text' => $answer ]
