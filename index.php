7723715bf89a16136f5f253d70e98e1d3df85df1
jeldon123 <jelsasukeops@gmail.com>
2017-03-09 01:17:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,6 @@ $input = json_decode(file_get_contents('php://input'), true);
 $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
 $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
-
 $answer = "I don't understand. Ask me 'hi'.";
 if($messageText == "hi") {
     $answer = "Hello";
